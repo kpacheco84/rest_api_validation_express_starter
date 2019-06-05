ac61587873726f9ca944c9fbae2edb45b2cbbb93
kpacheco84 <45826075+kpacheco84@users.noreply.github.com>
2019-06-05 01:08:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rest_api_validation_express_starter
---
+++ routes.js
@@ -0,0 +1,58 @@
+'use strict';
+//validation routers
+
+
+
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+// This array is used to keep track of user records
+// as they are created.
+const users = [];
+
+// Construct a router instance.
+const router = express.Router();
+
+
+
+// Route that returns a list of users.
+router.get('/users', (req, res) => {
+  res.json(users);
+});
+
+
+
+
+
+// Route that creates a new user.
+router.post('/users', [
+  check('name')
+    .exists()
+    .withMessage('Please provide a value for "name"'),
+  check('email')
+    .exists()
+    .withMessage('Please provide a value for "email"'),
+], (req, res) => {
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
+
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors.array().map(error => error.msg);
+
+    // Return the validation errors to the client.
+    res.status(400).json({ errors: errorMessages });
+  } else {
+    // Get the user from the request body.
+    const user = req.body;
+
+    // Add the user to the `users` array.
+    users.push(user);
+
+    // Set the status to 201 Created and end the response.
+    res.status(201).end();
+  }
+});
+
+
+module.exports = router;
